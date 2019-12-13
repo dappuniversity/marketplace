1ca4848caa55c751a728ce4a6ee88c2bbe4511af
codenamejason <codenamejason@outlook.com>
2019-12-12 19:18:28 -0500

    get local gnache working :)

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -30,7 +30,7 @@ contract Marketplace {
     );
 
     constructor() public {
-        name = "Dapp University Marketplace";
+        name = "Agency Marketplace";
     }
 
     function createProduct(string memory _name, uint _price) public {
