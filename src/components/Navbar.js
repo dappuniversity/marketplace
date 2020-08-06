6aa15bd225907f85e7214770643ea50824f8449c
Mahdi321 <mahg361@gmail.com>
2020-08-06 09:35:36 +0430

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dcensus.info"
+          href="http://www.dcensus.site"
           target="_blank"
           rel="noopener noreferrer"
         >
